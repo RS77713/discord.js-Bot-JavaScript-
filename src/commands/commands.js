0d6accf290da172197e782b65faf16ccbb2b2f98
Edgars Mihailovskis <61424717+RS77713@users.noreply.github.com>
2023-10-26 18:17:25 +0300

    Add files via upload

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,1 +1,28 @@
+const { Constants } = require('discord.js');
 
+module.exports = [{
+    name: 'ping',
+    description: 'Replies with Pong!',
+    action: async function (interaction, options) {
+        return interaction.reply({
+            content: 'Pong!'
+        });
+    }
+},
+{
+    name: 'pokemon',
+    description: 'Sends a pokemon card to a user',
+    options: [{
+        name: 'id',
+        description: 'Pokemon ID',
+        required: true,
+        type: Constants.ApplicationCommandOptionTypes.STRING
+    }],
+    action: async function (interaction, options) {
+        const id = options.getString('id');
+
+        return interaction.reply({
+            content: `https://images.pokemontcg.io/${id}/1_hires.png`
+        })
+    }
+}]
---
+++ src/commands/pokemons.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'pokemon',
+    description: 'Sends a pokemon card to a user',
+    options: [{
+        name: 'id',
+        this.description: ''
+    }]
+}
